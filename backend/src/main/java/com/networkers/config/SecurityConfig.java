a81ac72c152c9565bcaf7b8f15dc15e3cb6d3353
harshitSoft <harshit.nigam@itsoftlab.com>
2026-07-21 12:30:43 +0530

    new origin

--- backend/src/main/java/com/networkers/config/SecurityConfig.java
+++ backend/src/main/java/com/networkers/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("http://localhost:5173" ,"https://networkers-new.netlify.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
